882727fd7e9cecb48157fcd5055509296f04bc5f
haritsrs <haritssetiono23@gmail.com>
2024-10-09 08:22:32 +0700

    sidebar 0.2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,5 @@
-import localFont from "next/font/local";
 import "./globals.css";
-import Sidebar from '../components/Sidebar';
-
-// Importing custom fonts
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Sidebar from '../components/sidebar';
 
 // Defining metadata
 export const metadata = {
@@ -24,9 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <div className="flex">
           <Sidebar />
           <main className="flex-grow p-6">
