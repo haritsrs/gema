2f72d087318be80610bc03b99b1c488721d7e18f
Rafli Raihan <mrafliraihan@proton.me>
2024-10-19 14:12:27 +0000

    Layout Hotfix 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+
         <div className="flex min-h-screen">
 
           <aside className="hidden md:block fixed top-0 left-0 h-full w-max bg-purple-800 text-white p-4">
@@ -41,7 +43,7 @@ export default function RootLayout({ children }) {
           
 
           <div className="parent-div flex-col grow bg-gray-900 items-center justify-center overflow-y-auto mb-10 md:mb-0 md:pl-40">
-             <div className="md:mx-[18%] p-4 flex items-center justify-between">
+             <div className="p-4 flex items-center justify-between">
                 <img src="/img/logo.png" alt="GEMA Logo" className="w-50 h-8" />
 
                 <button className="text-white text-3xl hover:text-purple-800" onClick={toggleVisibility}>
@@ -56,7 +58,7 @@ export default function RootLayout({ children }) {
           {isVisible &&(
             <div className="fixed flex z-50 fixed h-screen w-screen bg-gray-950 bg-opacity-85 text-white p-4 justify-center">
 
-              <div className="relative flex-col w-[98%] h-[98%] max-w-sm bg-gray-900 rounded-xl overflow-y-auto">
+              <div className="relative flex-col w-[98%] h-[98%] max-w-sm bg-gray-900 rounded-xl items-center overflow-y-auto">
 
                 <button className="absolute text-bold text-white top-2 right-2 w-8 h-8 rounded-xl bg-gray-800" onClick={toggleVisibility}>
                   x
