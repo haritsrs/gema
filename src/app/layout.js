cf31b1ec06b4a2a78d2bbaf944e908b69551de86
Rafli Raihan <mrafliraihan@proton.me>
2024-10-20 17:02:21 +0000

    Major UI Tweaks 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,31 +32,18 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} className="bg-gray-900">
 
+         <div className="fixed w-screen top-0 p-4 flex items-center justify-between bg-gray-900 border border-b-1 border-gray-800 z-40">
+            <img src="/img/logo.png" alt="GEMA Logo" className="w-50 h-8" />
 
-        <div className="flex min-h-screen">
+            <button className="text-white text-3xl hover:text-purple-800" onClick={toggleVisibility}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M15 2h-1c-2.828 0-4.243 0-5.121.879C8 3.757 8 5.172 8 8v8c0 2.828 0 4.243.879 5.121C9.757 22 11.172 22 14 22h1c2.828 0 4.243 0 5.121-.879C21 20.243 21 18.828 21 16V8c0-2.828 0-4.243-.879-5.121C19.243 2 17.828 2 15 2" opacity={0.6}></path><path fill="white" d="M8 8c0-1.538 0-2.657.141-3.5H8c-2.357 0-3.536 0-4.268.732S3 7.143 3 9.5v5c0 2.357 0 3.535.732 4.268S5.643 19.5 8 19.5h.141C8 18.657 8 17.538 8 16z" opacity={0.4}></path><path fill="white" fillRule="evenodd" d="M14.53 11.47a.75.75 0 0 1 0 1.06l-2 2a.75.75 0 1 1-1.06-1.06l.72-.72H5a.75.75 0 0 1 0-1.5h7.19l-.72-.72a.75.75 0 1 1 1.06-1.06z" clipRule="evenodd"></path></svg>
+            </button>
+        </div>
 
-          <aside className="hidden md:block fixed top-0 left-0 h-full w-max bg-purple-800 text-white p-4">
-            <Sidebar />
-          </aside>
-          
-
-          <div className="parent-div flex-col grow bg-gray-900 items-center justify-center overflow-y-auto mb-10 md:mb-0 md:pl-40">
-             <div className="p-4 flex items-center justify-between">
-                <img src="/img/logo.png" alt="GEMA Logo" className="w-50 h-8" />
-
-                <button className="text-white text-3xl hover:text-purple-800" onClick={toggleVisibility}>
-                  <FontAwesomeIcon icon={faUserCircle} />
-                </button>
-             </div>
-
-            {children}
-
-          </div>
-
-          {isVisible &&(
-            <div className="fixed flex z-50 fixed h-screen w-screen bg-gray-950 bg-opacity-85 text-white p-4 justify-center">
+        {isVisible &&(
+            <div className="fixed flex z-50 h-screen w-screen bg-gray-950 bg-opacity-85 text-white p-4 justify-center">
 
               <div className="relative flex-col w-[98%] h-[98%] max-w-sm bg-gray-900 rounded-xl items-center overflow-y-auto">
 
@@ -65,20 +52,30 @@ export default function RootLayout({ children }) {
                 </button>
 
                 <AuthSidebar />
-
               </div>
-
-
             </div>
           )}
+
+        <div className="flex min-h-screen">
+
+          <div className="hidden md:block fixed top-16 left-2 h-full w-30 items-center justify-center bg-gray-950 outline outline-3 rounded-lg outline-gray-700 hover:outline-purple-800 text-white p-4 m-4">
+            <Sidebar />
+          </div>
+          
+
+          <div className="py-16 flex-col grow items-center justify-center focus:overflow-y-auto md:pb-0">
+            {children}
+          </div>
          
 
           {/* Mobile Navbar */}
-          <div className="md:hidden fixed bg-purple-800 bottom-0 left-0 w-screen text-white p-2">
+          <div className="md:hidden fixed bg-gray-900 bottom-0 left-0 w-screen h-max items-center text-white p-2 border border-t-1 border-gray-800">
             <Sidebar />
           </div> 
-
         </div>
+
+        
+
       </body>
     </html>
   );
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -16,35 +16,42 @@ export const metadata = {
 
 const Sidebar = () => {
   return (
-    <div className= "flex justify-between pl-4 pr-4 text-white md:flex-col md:pl-0 md:pr-0 md:py-4">
+    <div className= "flex justify-between pl-4 pr-4 text-white text-sm items-center md:flex-col md:pl-0 md:pr-0 md:p-4">
       <Link href="/" passHref>
-        <div className="md:flex items-center mb-0 md:mb-6">
-          <FontAwesomeIcon icon={faHome} size="2x" />
-          <span className="hidden md:block ml-2">Home</span>
+        <div className="flex flex-col items-center justify-center mb-0 rounded-lg md:mb-6 md:hover:bg-gray-800">
+          <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="white" d="M13.106 22h-2.212c-3.447 0-5.17 0-6.345-1.012s-1.419-2.705-1.906-6.093l-.279-1.937c-.38-2.637-.57-3.956-.029-5.083s1.691-1.813 3.992-3.183l1.385-.825C9.8 2.622 10.846 2 12 2s2.199.622 4.288 1.867l1.385.825c2.3 1.37 3.451 2.056 3.992 3.183s.35 2.446-.03 5.083l-.278 1.937c-.487 3.388-.731 5.081-1.906 6.093S16.553 22 13.106 22" opacity={0.5}></path><path fill="white" d="M8.25 18a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75"></path></svg> 
+          <span className="md:hidden">Home</span>
         </div>
       </Link>
       <Link href="/search" passHref>
-        <div className="md:flex items-center mb-0 md:mb-6">
-          <FontAwesomeIcon icon={faSearch} size="2x" />
-          <span className="hidden md:block ml-2">Search</span>
-        </div>
-      </Link>
-      <Link href="/notifications" passHref>
-        <div className="md:flex items-center mb-0 md:mb-6">
-          <FontAwesomeIcon icon={faBell} size="2x" />
-          <span className="hidden md:block ml-2">Notifications</span>
-        </div>
-      </Link>
-      <Link href="/messages" passHref>
-        <div className="flex items-center mb-0 md:mb-6">
-          <FontAwesomeIcon icon={faComment} size="2x" />
-          <span className="hidden md:block ml-2">Messages</span>
+        <div className="flex flex-col items-center justify-center mb-0 md:mb-6">
+          <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" className="flex"><path fill="white" d="M20.313 11.157a9.157 9.157 0 1 1-18.313 0a9.157 9.157 0 0 1 18.313 0" opacity={0.5}></path><path fill="white" fillRule="evenodd" d="M18.839 18.839a.723.723 0 0 1 1.022 0l1.928 1.927a.723.723 0 0 1-1.023 1.023L18.84 19.86a.723.723 0 0 1 0-1.022" clipRule="evenodd"></path></svg>
+          <span className="md:hidden">Search</span>
         </div>
       </Link>
       <Link href="/explore" passHref>
-        <div className="md:flex items-center mb-0 md:mb-6">
-          <FontAwesomeIcon icon={faGlobe} size="2x" />
-          <span className="hidden md:block ml-2">Explore</span>
+        <div className="flex flex-col items-center justify-center mb-0 md:mb-6">
+          <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="white" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2" className="duoicon-secondary-layer" opacity={0.3}></path><path fill="white" d="M16.243 7.757c-.354-.353-4.95.707-6.364 2.122c-1.414 1.414-2.475 6.01-2.122 6.364c.354.353 4.95-.707 6.364-2.122c1.415-1.414 2.475-6.01 2.122-6.364" className="duoicon-primary-layer"></path></svg>
+          <span className="md:hidden">Explore</span>
+        </div>
+      </Link>
+      <Link href="/messages" passHref>
+        <div className="flex flex-col items-center justify-center mb-0 md:mb-6">
+          <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="white" fillRule="evenodd" d="M2 6a3 3 0 0 1 3-3h14a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3H7.333L4 21.5c-.824.618-2 .03-2-1z" className="duoicon-secondary-layer" opacity={0.3}></path><path fill="white" fillRule="evenodd" d="M8 12a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2zM7 9a1 1 0 0 1 1-1h8a1 1 0 1 1 0 2H8a1 1 0 0 1-1-1" className="duoicon-primary-layer"></path></svg>
+          <span className="md:hidden text-center">Message</span>
+        </div>
+      </Link>
+      <Link href="/profile" passHref>
+        <div className="flex flex-col items-center justify-center mb-0 md:mb-6 hover:bg-gray-950 hover:rounded-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+            <path fill="white" d="M12 13c2.396 0 4.575.694 6.178 1.671c.8.49 1.484 1.065 1.978 1.69c.486.616.844 1.352.844 2.139c0 .845-.411 1.511-1.003 1.986c-.56.45-1.299.748-2.084.956c-1.578.417-3.684.558-5.913.558s-4.335-.14-5.913-.558c-.785-.208-1.524-.506-2.084-.956C3.41 20.01 3 19.345 3 18.5c0-.787.358-1.523.844-2.139c.494-.625 1.177-1.2 1.978-1.69C7.425 13.694 9.605 13 12 13" className="duoicon-primary-layer">
+            </path>
+            <path fill="white" d="M12 2c3.849 0 6.255 4.167 4.33 7.5A5 5 0 0 1 12 12c-3.849 0-6.255-4.167-4.33-7.5A5 5 0 0 1 12 2" className="duoicon-secondary-layer flex" opacity={0.3}>
+            </path>
+          </svg>
+          <span className="md:hidden text-center">
+            Profile
+          </span>
         </div>
       </Link>
     </div>
