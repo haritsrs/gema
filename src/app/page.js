bb5ed1e03d63a45bd9ac5e44884a0004ebf19d3d
rafli-raihan <mrafliraihan@proton.me>
2024-11-10 17:21:53 +0700

    svg fix 40%

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import Link from 'next/link';
 import LoadingOverlay from '../components/LoadingOverlay';
 import { usePostSystem } from '../hooks/usePostSystem';
 import PostDropdown from '../components/PostDropdown';
-import { HeartIcon, CommentIcon, ShareIcon } from '@/icons';
+import { HeartIcon, CommentIcon, ShareIcon, Loading } from '@/icons';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -184,7 +184,7 @@ export default function Page() {
                         : 'bg-gray-700 fill-gray-500'
                         }`}
                     >
-                      <HeartIcon className="w-5 h-5"
+                      <HeartIcon className="w-5 h-5" />
                       <span>{post.likes || 0}</span>
                     </button>
 
@@ -221,15 +221,12 @@ export default function Page() {
                 disabled={loading}
                 className={`w-full max-w-md py-3 px-4 rounded-lg text-white font-medium transition-all duration-200 ${loading
                   ? 'bg-gray-700 cursor-not-allowed'
-                  : 'bg-purple-600 hover:bg-purple-700 active:bg-purple-800'
+                  : 'bg-gray-700 active:bg-purple-800'
                   }`}
               >
                 {loading ? (
                   <div className="flex items-center justify-center space-x-2">
-                    <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
+                    <Loading />
                     <span className="text-white">Loading more posts...</span>
                   </div>
                 ) : (
--- src/icons/index.js
+++ src/icons/index.js
@@ -6,8 +6,9 @@ export { default as ProfilePlaceholder } from './svgs/profilePlaceholder.svg';
 export { default as HeartIcon } from './svgs/heart.svg';
 export { default as CommentIcon } from './svgs/comment.svg';
 export { default as ShareIcon } from './svgs/share.svg';
-{/*export { default as HomeIcon } from './svgs/home.svg';
+export { default as Loading } from './svgs/loading.svg';
+export { default as HomeIcon } from './svgs/home.svg';
 export { default as SearchIcon } from './svgs/search.svg';
 export { default as NotificationsIcon } from './svgs/notif.svg';
 export { default as ProfileIcon } from './svgs/profile.svg';
-export { default as SettingsIcon } from './svgs/settings.svg';*/}
+export { default as SettingsIcon } from './svgs/settings.svg';
