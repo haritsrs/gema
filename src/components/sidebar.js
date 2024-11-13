61d60d9211382dbac9e89b178583096bfa55f35a
Rafli Raihan <mrafliraihan@proton.me>
2024-11-13 20:07:25 +0700

    Update sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -13,9 +13,9 @@ export const metadata = {
 
 const Sidebar = () => {
   return (
-    <div className="flex justify-between pl-4 pr-4 text-white text-sm items-center md:flex-col md:pl-0 md:pr-0 md:p-4 fill-gray-400">
+    <div className="flex justify-between pl-4 pr-4 text-white text-sm items-center md:flex-col md:pl-0 md:pr-0 md:p-4">
       <Link href="/" passHref>
-        <div className="flex flex-col items-center justify-center mb-0 md:mb-6 active:bg-purple-400 active:fill-purple-800 active:stroke-purple-800 rounded-lg">
+        <div className="flex flex-col items-center justify-center mb-0 md:mb-6 fill-gray-400 active:bg-purple-400 active:fill-purple-800 active:stroke-purple-800 rounded-lg">
           <svg xmlns="http://www.w3.org/2000/svg" width="1.75em" height="1.75em" viewBox="0 0 24 24"><path d="M13.106 22h-2.212c-3.447 0-5.17 0-6.345-1.012s-1.419-2.705-1.906-6.093l-.279-1.937c-.38-2.637-.57-3.956-.029-5.083s1.691-1.813 3.992-3.183l1.385-.825C9.8 2.622 10.846 2 12 2s2.199.622 4.288 1.867l1.385.825c2.3 1.37 3.451 2.056 3.992 3.183s.35 2.446-.03 5.083l-.278 1.937c-.487 3.388-.731 5.081-1.906 6.093S16.553 22 13.106 22" opacity={0.5}></path><path fill="white" d="M8.25 18a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75"></path></svg>
           <span className="md:hidden m-1 active:text-purple-900">Home</span>
         </div>
