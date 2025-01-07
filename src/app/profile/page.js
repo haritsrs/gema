9befe9f4da4f171d59f47ef0186113ec3eec2e8d
haritsrs <haritssetiono23@gmail.com>
2025-01-07 11:52:10 +0700

    profile reroute fix

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,6 +2,7 @@
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { auth } from '../../../firebase.js';
+import { onAuthStateChanged } from 'firebase/auth';
 import AuthSidebar from '../../components/auth';
 
 export default function ProfileRedirect() {
@@ -9,12 +10,16 @@ export default function ProfileRedirect() {
   const [showAuthSidebar, setShowAuthSidebar] = useState(false);
 
   useEffect(() => {
-    const user = auth.currentUser;
-    if (user) {
-      router.replace(`/profile/${user.uid}`);
-    } else {
-      setShowAuthSidebar(true);
-    }
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.replace(`/profile/${user.uid}`);
+      } else {
+        setShowAuthSidebar(true);
+      }
+    });
+
+    // Cleanup subscription
+    return () => unsubscribe();
   }, [router]);
 
   if (showAuthSidebar) {
